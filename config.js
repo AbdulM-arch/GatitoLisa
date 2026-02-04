2487d578a8d72ba7d268e95ee8aa493602357de5
AbdulM-arch <afnan.msr3@gmail.com>
2026-02-04 16:21:02 +0400

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "http://online.kissfm.ua/KissFM_Ukr", // Music streaming URL
+        musicUrl: "https://www.dropbox.com/scl/fi/n0478r9s3l6jmszvdv2j4/Ghost-Duet-Louie-Zong.mp3?rlkey=c2mbw96uadj6gp2gsxwo6jbxk&st=8jn0ky77&dl=0", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
