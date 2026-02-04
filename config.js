51903cd6e6753361f05fe6d14e75a3a57ca875b3
AbdulM-arch <afnan.msr3@gmail.com>
2026-02-04 15:57:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://audio.com/afnan-msr3/audio/ghost-duet-louie-zong", // Music streaming URL
+        musicUrl: "http://online.kissfm.ua/KissFM_Ukr", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
